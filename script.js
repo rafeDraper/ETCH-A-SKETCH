b8f4ce842d8dc6ec3023e00363b5b0631de9a6c7
rafeDraper <rafedraper@gmail.com>
2019-02-27 10:07:24 +0100

    Updates

---
+++ script.js
@@ -0,0 +1,7 @@
+let container = document.querySelector('#main');
+let div = document.createElement('div');
+div.classList.add('first')
+div.textContent = "Hello Culo";
+
+
+container.appendChild(div);
